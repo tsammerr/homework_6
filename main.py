388698ebdfc6b7d17cc2054e90273a3bb720448e
Maria <tarasovych.maria@gmail.com>
2022-10-18 22:46:09 +0300

    init: ex2

---
+++ main.py
@@ -0,0 +1,21 @@
+try:
+    sign input('sign->*)
+
+    Lenght = int(input('Lenght->')) else:
+
+    trigger input('trigger [vertical v horizontal-h]->')
+    for iten in range(0, lenght):
+        if trigger V:
+            print(sign)
+
+        elif trigger'h': print(sign, ende)
+
+    raise Exception
+    Encorrect
+    choose
+    of
+    trigger
+    ')
+
+
+except Exception as ox: print (f'Erorr information: fex)')
