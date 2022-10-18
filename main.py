20aceb3f9daafaf3c7d5859dd54a7c644464cacf
Maria <tarasovych.maria@gmail.com>
2022-10-18 22:54:22 +0300

    ex2

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 try:
-    sign input('sign->*)
-
-    Lenght = int(input('Lenght->')) else:
-
-    trigger input('trigger [vertical v horizontal-h]->')
-    for iten in range(0, lenght):
-        if trigger V:
+    sign = input('sign-> ')
+    lenght = int(input('Lenght-> '))
+    trigger = input('trigger [vertical - v | horizontal - h] -> ')
+    for item in range(0, lenght):
+        if trigger == 'v':
             print(sign)
+        elif trigger == 'h':
+            print(sign, end='')
+        else:
+            raise Exception ('Incorrect choose of trigger')
 
-        elif trigger'h': print(sign, ende)
-
-    raise Exception
-    Encorrect
-    choose
-    of
-    trigger
-    ')
-
-
-except Exception as ox: print (f'Erorr information: fex)')
+except Exception as ex:
+    print (f'Error information: {ex}')
